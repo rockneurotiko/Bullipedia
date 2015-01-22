76890389aaa00bd9831fde31ef38049c4fe02887
Rock Neurotiko <miguelglafuente@gmail.com>
2015-01-22 12:59:15 +0100

    modified

--- carousel/js/setup.js
+++ carousel/js/setup.js
@@ -1,26 +1,30 @@
-var find_first = function(arr){
-    for (i in arr){
-        if(arr[i].trim() != "")
-            return arr[i].trim();    
-    }
-};
+// var find_first = function(arr){
+//   console.log(arr);
+//     for (i in arr){
+//       if(arr[i].trim() != ""){
+//         console.log(arr[i]);
+//             return arr[i].trim();
+//       }
+//     }
+// };
 
-var find_last = function(arr){
-    var last = "";
-    arr.forEach(function(n){
-        if(n.trim() != "")
-            last = n.trim();
-    });
-    return last;
-};
+// var find_last = function(arr){
+//   console.log(arr);
+//     var last = "";
+//     arr.forEach(function(n){
+//         if(n.trim() != "")
+//             last = n.trim();
+//     });
+//     return last;
+// };
 
-var move_f = function(func) {
-    var cent = $('.owl-item.active');
-    var l = func(cent.text().split('\n'));
-    var f = angular.element(cent).scope().update;
+// var move_f = function(func) {
+//     var cent = $('.owl-item.active');
+//     var l = func(cent.text().split('\n'));
+//     var f = angular.element(cent).scope().update;
     
-    f(l);
-};
+//   //f(l);
+// };
 
 
 $(document).ready(function(){    
@@ -33,28 +37,32 @@ $(document).ready(function(){
     animateOut: 'fadeOut',
     mouseDrag: false
   });
+
+  owl.on('changed.owl.carousel', function(event) {
+    var diff = event.isTrigger;
+    var n = event.item.index;
+    var n_f = Math.abs(diff-n);
+    var cent = $('.owl-item.active');
+    var f = angular.element(cent).scope().update_n;
+    f(n_f);
+  });
   
   $(".link").click(function(e){ 
     var cent_ind = $('.owl-item.active.center').index();
       var ind = $(this).parent().index();
-      console.log(ind);
     if (cent_ind < ind) {
-      owl.trigger('next.owl.carousel');
-      move_f(find_last);
+      owl.trigger('next.owl.carousel',[200]);
     } else if (cent_ind > ind) {
-      owl.trigger ('prev.owl.carousel');
-      move_f(find_first);
+      owl.trigger ('prev.owl.carousel',[200]);
     }
   });
 
   $(".owl-next").click(function(){
-    owl.trigger('next.owl.carousel');
-    move_f(find_last);
+    owl.trigger('next.owl.carousel',[200]);
   });
 
   $(".owl-prev").click(function(){
-    owl.trigger('prev.owl.carousel');
-    move_f(find_first);
+    owl.trigger('prev.owl.carousel',[200]);
   });
     
 });
--- carousel/test.js
+++ carousel/test.js
@@ -6,11 +6,11 @@ var DemoCtrl = function($scope) {
                 'FRUIT': ['Naranja','Mandarina','Manzana','Pera','Platano'],
                 'MEAT': ['Vaca', 'Ternera', 'Pollo', 'Buey', 'Jabali'],
                 'CEREALS': ['Cereals0','Cereals1','Cereals2','Cereals3','Cereals4','Cereals5'],
-                'DAIRY': ['Dairy0','Dairy1','Dairy2','Dairy3','Dairy4','Dairy5'],
+                'DAIRYS': ['Dairy0','Dairy1','Dairy2','Dairy3','Dairy4','Dairy5'],
                 'FISH': ['Fish0','Fish1','Fish2','Fish3','Fish4','Fish5'],
                 'MILK': ['Milk0','Milk1','Milk2','Milk3','Milk4','Milk5']};
 
-    var urls = ["img/veg.svg", "img/fruit.svg","img/meat.svg", "img/cereals.svg", "img/dairy.svg","img/fish.svg", "img/milk.svg"];
+    var urls = ["img/veg.svg", "img/fruit.svg","img/meat.svg", "img/cereals.svg", "img/dairys.svg","img/fish.svg", "img/milk.svg"];
 
   var mythis = this;
   angular.forEach(urls, function(url){
@@ -24,19 +24,34 @@ var DemoCtrl = function($scope) {
   });
 
     this.selected = this.types[this.images[0].name];
-    this.name_selected = this.images[0].name;
+  this.name_selected = this.images[0].name;
     
   $scope.update = function(name){
+    $scope.$apply(function(){  
       mythis.selected = mythis.types[name];
       mythis.name_selected = name;
-      $scope.$apply();
+    });
   };
 
-    $scope.select = function(image){
-        if(image.name === mythis.name_selected){
-            return image.image.split(".")[0] + "_color.svg";}
-        return image.image;
-    };
+  $scope.update_n = function(n){
+    $scope.$apply(function(){
+      var selected = urls[n].split("/")[1].split(".")[0].toUpperCase();
+      mythis.select = mythis.types[selected];
+      mythis.name_selected = selected;
+    });
+  };
+
+  $scope.select = function(image,u){
+    if(image.name === mythis.name_selected){
+      return image.image.split(".")[0] + "_color.svg";
+    }
+    return image.image;
+  };
+  
+  $scope.selected = function(image){
+    //console.log(mythis.name_selected);
+    return image.name === mythis.name_selected;
+  };
 };
 
 app.controller("DemoCtrl", ['$scope', DemoCtrl]);
